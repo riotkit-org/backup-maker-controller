2efc368d89b89c689380c66b523b3f9e0ab18ff8
Andrew Johnson <br@example.org>
2022-11-12 13:25:25 +0100

    refactor: Move from logr to logrus (GH #6)

---
+++ cmd/root.go
@@ -0,0 +1,193 @@
+package cmd
+
+import (
+	riotkitorgv1alpha1 "github.com/riotkit-org/backup-maker-operator/pkg/apis/riotkit/v1alpha1"
+	"github.com/riotkit-org/backup-maker-operator/pkg/client/clientset/versioned/typed/riotkit/v1alpha1"
+	controllers2 "github.com/riotkit-org/backup-maker-operator/pkg/controllers"
+	"github.com/riotkit-org/backup-maker-operator/pkg/factory"
+	"github.com/riotkit-org/backup-maker-operator/pkg/integration"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/dynamic"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+func NewRootCommand() *cobra.Command {
+	app := App{}
+	command := &cobra.Command{
+		Use:   "backup-maker-operator",
+		Short: "Runs a controller that schedules and monitors Backup & Restore actions in the cluster",
+		Run: func(command *cobra.Command, args []string) {
+			err := app.Run()
+
+			if err != nil {
+				logrus.Errorf(err.Error())
+				os.Exit(1)
+			}
+		},
+	}
+
+	command.Flags().BoolVarP(&app.debug, "debug", "v", true, "Increase verbosity to the debug level")
+	command.Flags().StringVarP(&app.metricsBindAddress, "metrics-bind-address", "m", ":8080", "Host + Port on which to bind metrics endpoint to")
+	command.Flags().StringVarP(&app.healthProbeBindAddress, "health-probe-bind-address", "p", ":8081", "Host + Port on which to bind healthcheck endpoint to")
+	command.Flags().BoolVarP(&app.leaderElect, "leader-elect", "l", false, "Enable leader election? Use in HA mode")
+	command.Flags().BoolVarP(&app.zapDevel, "zap-devel", "", false, "Enable development mode for Zap")
+
+	return command
+}
+
+type App struct {
+	debug                  bool
+	metricsBindAddress     string
+	healthProbeBindAddress string
+	leaderElect            bool
+	zapDevel               bool
+}
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(riotkitorgv1alpha1.AddToScheme(scheme))
+	// +kubebuilder:scaffold:scheme
+}
+
+func (a *App) Run() error {
+	if a.debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	// zap logger is used by KubeBuilder
+	ctrl.SetLogger(zap.New(
+		zap.UseDevMode(a.zapDevel),
+		zap.ConsoleEncoder(),
+	))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                        scheme,
+		MetricsBindAddress:            a.metricsBindAddress,
+		Port:                          9443,
+		HealthProbeBindAddress:        a.healthProbeBindAddress,
+		LeaderElection:                a.leaderElect,
+		LeaderElectionID:              "7b93e0c3.riotkit.org",
+		LeaderElectionReleaseOnCancel: true,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+	recorder := mgr.GetEventRecorderFor("backup-maker-operator")
+	kubeconfig, err := buildConfig(os.Getenv("KUBECONFIG"))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// check Kubernetes connection
+	dynClient, clErr := dynamic.NewForConfig(kubeconfig)
+	if clErr != nil {
+		panic(clErr.Error())
+	}
+	brClient, clErr := v1alpha1.NewForConfig(kubeconfig)
+	if clErr != nil {
+		panic(clErr.Error())
+	}
+	integrations := integration.NewAllSupportedJobResourceTypes(kubeconfig)
+	fetcher := factory.CachedFetcher{Cache: mgr.GetCache(), Client: brClient}
+
+	if err = (&controllers2.ClusterBackupProcedureTemplateReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+		Cache:  mgr.GetCache(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ClusterBackupProcedureTemplate")
+		return err
+	}
+	if err = (&controllers2.ScheduledBackupReconciler{
+		Client:    mgr.GetClient(),
+		Scheme:    mgr.GetScheme(),
+		Cache:     mgr.GetCache(),
+		BRClient:  brClient,
+		RestCfg:   kubeconfig,
+		DynClient: dynClient,
+		Fetcher:   factory.CachedFetcher{Cache: mgr.GetCache(), Client: brClient},
+		Recorder:  recorder,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ScheduledBackup")
+		return err
+	}
+	if err = (&controllers2.RequestedBackupActionReconciler{
+		Client:    mgr.GetClient(),
+		Scheme:    mgr.GetScheme(),
+		Cache:     mgr.GetCache(),
+		BRClient:  brClient,
+		DynClient: dynClient,
+		RestCfg:   kubeconfig,
+		Fetcher:   fetcher,
+		Recorder:  recorder,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "RequestedBackupAction")
+		return err
+	}
+	// +kubebuilder:scaffold:builder
+	if err = (&controllers2.JobsManagedByRequestedBackupActionObserver{
+		Integrations: &integrations,
+		Fetcher:      fetcher,
+		BRClient:     brClient,
+		Client:       mgr.GetClient(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "JobsManagedByRequestedBackupActionObserver")
+		return err
+	}
+	if err = (&controllers2.JobsManagedByScheduledBackupObserver{
+		Integrations: &integrations,
+		Fetcher:      fetcher,
+		BRClient:     brClient,
+		Client:       mgr.GetClient(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "JobsManagedByScheduledBackupObserver")
+		return err
+	}
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		return err
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		return err
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		return err
+	}
+	return nil
+}
+
+func buildConfig(kubeconfig string) (*rest.Config, error) {
+	if kubeconfig != "" {
+		cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, err
+		}
+		return cfg, nil
+	}
+
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
--- main.go
+++ main.go
@@ -17,179 +17,21 @@ limitations under the License.
 package main
 
 import (
-	"flag"
-	riotkitorgv1alpha1 "github.com/riotkit-org/backup-maker-operator/pkg/apis/riotkit/v1alpha1"
-	"github.com/riotkit-org/backup-maker-operator/pkg/client/clientset/versioned/typed/riotkit/v1alpha1"
-	controllers2 "github.com/riotkit-org/backup-maker-operator/pkg/controllers"
-	"github.com/riotkit-org/backup-maker-operator/pkg/factory"
-	"github.com/riotkit-org/backup-maker-operator/pkg/integration"
-	"github.com/sirupsen/logrus"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
+	"github.com/riotkit-org/backup-maker-operator/cmd"
 	"os"
-
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	// +kubebuilder:scaffold:imports
 )
-
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
-
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	utilruntime.Must(riotkitorgv1alpha1.AddToScheme(scheme))
-	// +kubebuilder:scaffold:scheme
-}
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
+	command := cmd.NewRootCommand()
+	args := os.Args
+
+	if args != nil {
+		args = args[1:]
+		command.SetArgs(args)
 	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
 
-	// todo: fix together with https://github.com/riotkit-org/backup-maker-operator/issues/6
-	// turn on debug also globally in the logrus
-	//if opts.Level.Enabled(zapcore.DebugLevel) {
-	logrus.SetLevel(logrus.DebugLevel)
-	//}
-
-	// our CRD client
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                        scheme,
-		MetricsBindAddress:            metricsAddr,
-		Port:                          9443,
-		HealthProbeBindAddress:        probeAddr,
-		LeaderElection:                enableLeaderElection,
-		LeaderElectionID:              "7b93e0c3.riotkit.org",
-		LeaderElectionReleaseOnCancel: true,
-	})
+	err := command.Execute()
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
-	recorder := mgr.GetEventRecorderFor("backup-maker-operator")
-	kubeconfig, err := buildConfig(os.Getenv("KUBECONFIG"))
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// check Kubernetes connection
-	dynClient, clErr := dynamic.NewForConfig(kubeconfig)
-	if clErr != nil {
-		panic(clErr.Error())
-	}
-	brClient, clErr := v1alpha1.NewForConfig(kubeconfig)
-	if clErr != nil {
-		panic(clErr.Error())
-	}
-	integrations := integration.NewAllSupportedJobResourceTypes(kubeconfig)
-	fetcher := factory.CachedFetcher{Cache: mgr.GetCache(), Client: brClient}
-
-	if err = (&controllers2.ClusterBackupProcedureTemplateReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-		Cache:  mgr.GetCache(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "ClusterBackupProcedureTemplate")
-		os.Exit(1)
-	}
-	if err = (&controllers2.ScheduledBackupReconciler{
-		Client:    mgr.GetClient(),
-		Scheme:    mgr.GetScheme(),
-		Cache:     mgr.GetCache(),
-		BRClient:  brClient,
-		RestCfg:   kubeconfig,
-		DynClient: dynClient,
-		Fetcher:   factory.CachedFetcher{Cache: mgr.GetCache(), Client: brClient},
-		Recorder:  recorder,
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "ScheduledBackup")
-		os.Exit(1)
-	}
-	if err = (&controllers2.RequestedBackupActionReconciler{
-		Client:    mgr.GetClient(),
-		Scheme:    mgr.GetScheme(),
-		Cache:     mgr.GetCache(),
-		BRClient:  brClient,
-		DynClient: dynClient,
-		RestCfg:   kubeconfig,
-		Fetcher:   fetcher,
-		Recorder:  recorder,
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "RequestedBackupAction")
-		os.Exit(1)
-	}
-	// +kubebuilder:scaffold:builder
-	if err = (&controllers2.JobsManagedByRequestedBackupActionObserver{
-		Integrations: &integrations,
-		Fetcher:      fetcher,
-		BRClient:     brClient,
-		Client:       mgr.GetClient(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "JobsManagedByRequestedBackupActionObserver")
-		os.Exit(1)
-	}
-	if err = (&controllers2.JobsManagedByScheduledBackupObserver{
-		Integrations: &integrations,
-		Fetcher:      fetcher,
-		BRClient:     brClient,
-		Client:       mgr.GetClient(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "JobsManagedByScheduledBackupObserver")
-		os.Exit(1)
-	}
-
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up health check")
-		os.Exit(1)
-	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up ready check")
-		os.Exit(1)
-	}
-
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
-		os.Exit(1)
-	}
-}
-
-func buildConfig(kubeconfig string) (*rest.Config, error) {
-	if kubeconfig != "" {
-		cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-		if err != nil {
-			return nil, err
-		}
-		return cfg, nil
-	}
-
-	cfg, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, err
-	}
-	return cfg, nil
 }
